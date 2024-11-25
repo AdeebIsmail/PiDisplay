43b0d605e30ce0abac2fe9485526262db7886397
Adeeb Ismail <adeebi5412@protonmail.com>
2024-11-25 12:36:25 -0600

    Fixed spotify timing out

--- data/musicdata.py
+++ data/musicdata.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import time
 import json
+import requests
 
 load_dotenv()
 scope = "user-library-read user-read-playback-state user-read-recently-played"
@@ -22,21 +23,33 @@ prev_song_name = None
 def musicData():
     global prev_song_name
 
-    song_info = sp.current_playback(market='US')
-    if (song_info is None):
-        return None
-    if (song_info['item'] is None):
-        return None
-    song_name = song_info['item']['name']
-    song_album_name = song_info['item']['album']['name']
-    song_art = song_info['item']['album']['images'][1]
-    song_artists = song_info['item']['artists']
-    song_artists_list = []
-    if (prev_song_name == song_name):
-        return None
+    try:
+        song_info = sp.current_playback(market='US')
+        if song_info is None or song_info['item'] is None:
+            return None
 
-    prev_song_name = song_name
+        song_name = song_info['item']['name']
+        song_album_name = song_info['item']['album']['name']
 
-    for artist in song_artists:
-        song_artists_list.append(artist['name'])
-    return [song_name, song_artists_list, song_album_name, song_art]
+        if not song_info['item']['album']['images']:
+            return None
+        song_art = song_info['item']['album']['images'][1]
+
+        if not song_info['item']['artists']:
+            return None
+        song_artists = song_info['item']['artists']
+        song_artists_list = []
+
+        if prev_song_name == song_name:
+            return None
+
+        prev_song_name = song_name
+
+        for artist in song_artists:
+            song_artists_list.append(artist['name'])
+
+        return [song_name, song_artists_list, song_album_name, song_art]
+
+    except requests.exceptions.ReadTimeout:
+        print("Request to Spotify API timed out.")
+        return None
